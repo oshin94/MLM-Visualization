53418775dab8df42b1d303d40554200dcea5d374
Arnab Paul Choudhury <arnabpc.oshin94@gmail.com>
2020-07-25 21:05:54 +0530

    1st commit

---
+++ static/render.js
@@ -0,0 +1,9 @@
+window.onload=function() {
+    canv=document.getElementById("myCanvas");
+    ctx=canv.getContext("2d");
+	
+	ctx.fillStyle="black";
+	ctx.fillRect(0,0,canv.width,canv.height);
+
+}
+
